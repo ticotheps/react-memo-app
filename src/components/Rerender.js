4ce92cea55c65b819cd90453591a21425d6f8d27
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 03:25:00 -0500

    Adds a clarifying comment to explain what problem the new React.memo feature solves

--- src/components/Rerender.js
+++ src/components/Rerender.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Rerender({ seconds }) {
+	// This console.log() statement should should be printing every second
+	// in your console because this component is trying to update every
+	// second even though the 'seconds' prop that is being passed to it is
+	// hardcoded as a '1'.
+	console.log('I am rendering');
 	return <h3>I am updating every {seconds} seconds!</h3>;
 }
