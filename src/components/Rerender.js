03e5876d164fe43bde67cba3665877f537aff8e7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 03:27:16 -0500

    Adds more comments to explain how React.memo improves performance by preventing extra re-renders

--- src/components/Rerender.js
+++ src/components/Rerender.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Rerender({ seconds }) {
+function Rerender({ seconds }) {
 	// This console.log() statement should should be printing every second
 	// in your console because this component is trying to update every
 	// second even though the 'seconds' prop that is being passed to it is
@@ -8,3 +8,9 @@ export default function Rerender({ seconds }) {
 	console.log('I am rendering');
 	return <h3>I am updating every {seconds} seconds!</h3>;
 }
+
+export default Rerender;
+
+// Because this component continues to rerender despite receiving the same
+// exact hardcoded prop, it is doing extra work that it does not have to do
+// which decreases performance.
