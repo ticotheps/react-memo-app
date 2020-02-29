9898a26aa7c85e2acaef6e537c0655b5419feace
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 03:29:32 -0500

    Wraps the <Rerender /> component with 'React.memo()' to improve performance by preventing unnecessary extra re-renders

--- src/components/Rerender.js
+++ src/components/Rerender.js
@@ -9,7 +9,9 @@ function Rerender({ seconds }) {
 	return <h3>I am updating every {seconds} seconds!</h3>;
 }
 
-export default Rerender;
+// Wrapping your component with 'React.memo()' prevents the extra, unnecessary
+// re-renders to improve performance!
+export default React.memo(Rerender);
 
 // Because this component continues to rerender despite receiving the same
 // exact hardcoded prop, it is doing extra work that it does not have to do
