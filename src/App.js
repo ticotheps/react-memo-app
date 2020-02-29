8f4991ce2cf924ae201456f5158dac2441372a42
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-29 03:11:19 -0500

    Updates function-based component to a class-based component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function App() {
-	return (
-		<div>
-			<h1>REACT.MEMO TUTORIAL</h1>
-		</div>
-	);
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>REACT.MEMO TUTORIAL</h1>
+			</div>
+		);
+	}
 }
